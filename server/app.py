3dca5ace844413c4ca2e407f7047c8951955aef5
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-07-06 21:35:44 +0300

    defined api methods

--- server/app.py
+++ server/app.py
@@ -17,11 +17,31 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        response_body =[plant.to_dict() for plant in plants]
+        return make_response(response_body,200)
+    def post(self):
+        data = request.get_json()
+        name = data.get('name')
+        image = data.get('image')
+        price = data.get('price')
+
+        if not name or not image or not price:
+            return make_response("All fields are required", 400)
+
+        plant = Plant(name=name, image=image, price=price)
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 201)
+api.add_resource(Plants,'/plants')  
 
 class PlantByID(Resource):
-    pass
+    def get(self, plant_id):
+        plant = Plant.query.get_or_404(plant_id)
+        return make_response(plant.to_dict(), 200)
         
-
+api.add_resource(PlantByID,'/plants/<int:plant_id>') 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
